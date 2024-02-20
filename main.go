6ddfa2607bf521c2cfe6b743f7026037e0308eeb
Jakub Dabrowski <kubada255@gmail.com>
2024-02-20 18:28:47 +0100

    First commit

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	htgotts "github.com/hegedustibor/htgo-tts"
+)
+
+func main() {
+	fmt.Println("Type something to say:")
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Type text")
+	text, _ := reader.ReadString('\n')
+
+	fmt.Println("Name of the file to save the speech to:")
+	var filename string
+	fmt.Scanln(&filename)
+	fmt.Println("You typed:", filename)
+	var jezyk string
+	fmt.Println("Type language")
+	fmt.Scanln(&jezyk)
+	speech := htgotts.Speech{Folder: "audio", Language: jezyk}
+
+	// Zapisywanie przemówienia pod konkretą nazwą pliku
+	speech.CreateSpeechFile(text, filename)
+	fmt.Println("Speech has been spoken and saved to:", filename)
+	fmt.Println("Press enter to exit")
+	fmt.Scanln()
+}
